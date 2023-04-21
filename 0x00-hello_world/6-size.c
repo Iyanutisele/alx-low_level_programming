8f3bbebec8836a514bb1b735525d0eb1c8c84ece
Hannah Afuape <bamigbadeolamide@gmail.com>
2023-04-21 07:46:31 -0700

    Size is not grandeur, and territory does not make a nation mandatory

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- *main - A program that prints the size of various computer types
+ * main - A program that prints the size of various computer types
  *
  *Return: 0 (Success)
  */
@@ -19,3 +19,4 @@ printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
+
