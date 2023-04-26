c9e28c38d1727a7088e370bf2c27150f4b620fe0
Hannah Afuape <bamigbadeolamide@gmail.com>
2023-04-26 11:22:51 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+void prints_alphabet(void)
+{
+int i;
+
+for (i = 97; i <= 122; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+return (0);
+}	
